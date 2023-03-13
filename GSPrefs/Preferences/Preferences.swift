1777dc603cba4a48002d4fea67578fd73dd77e42
ginsudev <noahlittle199@gmail.com>
2023-03-13 22:00:36 +1100

    Initial release

---
+++ GSPrefs/Preferences/Preferences.swift
@@ -0,0 +1,68 @@
+//
+//  Preferences.swift
+//  GSPrefs
+//
+//  Created by Noah Little on 11/3/2023.
+//
+
+import Foundation
+
+public struct Preferences {
+    /// Sets any Codable-conforming type as the value of a given key.
+    /// - Parameters:
+    ///   - value: The new value to set.
+    ///   - key: The key which the new value will be saved to.
+    ///   - registry: The path to the .plist
+    public static func setValue<T: Codable>(
+        _ value: T,
+        key: String,
+        registry: String
+    ) throws {
+        let plistURL = URL(fileURLWithPath: registry)
+        do {
+            var dict = plistURL.plistDict()
+            dict[key] = value
+            let dictData = try PropertyListSerialization.data(
+                fromPropertyList: dict,
+                format: .xml,
+                options: 0
+            )
+            try dictData.write(to: plistURL)
+        } catch {
+            throw error
+        }
+    }
+    
+    /// Retrieves a value from a given key within a .plist
+    /// - Parameters:
+    ///   - key: The key to retrieve the value for.
+    ///   - registry: The path to the .plist.
+    ///   - returnType: The `Type` that will be returned.
+    /// - Returns: A value casted to the `returnType`.
+    public static func value<T: Codable>(
+        key: String,
+        registry: String,
+        returnType: T.Type
+    ) -> T? {
+        let plistURL = URL(fileURLWithPath: registry)
+        let dict = plistURL.plistDict()
+        return dict[key] as? T
+    }
+}
+
+private extension URL {
+    func plistDict() -> [String: Any] {
+        var propertyListFormat: PropertyListSerialization.PropertyListFormat = .xml
+        do {
+            let data = try Data(contentsOf: self)
+            guard var plistDict = try PropertyListSerialization.propertyList(
+                from: data,
+                options: .mutableContainersAndLeaves,
+                format: &propertyListFormat
+            ) as? [String : Any] else { return [String : Any]() } // Return empty dictionary if plist doesn't exist yet.
+            return plistDict
+        } catch {
+            return [String : Any]()
+        }
+    }
+}
---
+++ GSPrefs/Preferences/Published+Preferences.swift
@@ -0,0 +1,44 @@
+//
+//  Published+Preferences.swift
+//  GSPrefs
+//
+//  Created by Noah Little on 12/3/2023.
+//
+
+import Foundation
+import Combine
+
+private var cancellableSet: Set<AnyCancellable> = []
+
+public extension Published where Value: Codable {
+    /// Notifies subscribers when changes occur in a .plist.
+    /// - Parameters:
+    ///   - defaultValue: The default / fallback value.
+    ///   - key: The key where the value will be read to & written from.
+    ///   - registry: The path to the .plist file containing preferences.
+    init(
+        wrappedValue defaultValue: Value,
+        key: String,
+        registry: String
+    ) {
+        if let value = Preferences.value(
+            key: key,
+            registry: registry,
+            returnType: Value.self
+        ) {
+            self.init(initialValue: value)
+        } else {
+            self.init(initialValue: defaultValue)
+        }
+        
+        projectedValue
+            .sink { value in
+                try? Preferences.setValue(
+                    value,
+                    key: key,
+                    registry: registry
+                )
+            }
+            .store(in: &cancellableSet)
+    }
+}
